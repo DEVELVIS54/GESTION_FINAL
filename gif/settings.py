375543c3658960eef36d246165097f2c00d33247
SEBGO ELVIS <sebgoelvis842@gmail.com>
2026-04-17 21:53:54 +0000

    fix settings

--- gif/settings.py
+++ gif/settings.py
@@ -4,9 +4,9 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+SECRET_KEY = os.environ.get('d$irgqs^op+=5okx1@ov@r11_q+t*86dbu^8j@o=2u8_q0@(+o')  # ← clé fixée
+DEBUG = os.environ.get('DEBUG') == 'True'  # ← 'True' pas 'Fals'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')  # ← ligne parasite supprimée
 
 INSTALLED_APPS = [
     'django.contrib.admin',
