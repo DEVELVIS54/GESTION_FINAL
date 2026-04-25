7eea053c49443d09ce7b9f404065a061f071a127
DEVELVIS54 <sebgoelvis842@gmail.com>
2026-04-25 21:49:58 +0000

    Update settings.py

--- gif/settings.py
+++ gif/settings.py
@@ -51,7 +51,9 @@ WSGI_APPLICATION = 'gif.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3'
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=False  # ← changer True en False
     )
 }
 
