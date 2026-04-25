e5bf2b61d28d10bb1aa82ca7352cb29fa6032fc7
DEVELVIS54 <sebgoelvis842@gmail.com>
2026-04-25 22:07:52 +0000

    Update settings.py

--- gif/settings.py
+++ gif/settings.py
@@ -5,8 +5,8 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'd$irgqs^op+=5okx1@ov@r11_q+t*86dbu^8j@o=2u8_q0@(+o')
-DEBUG = os.environ.get('DEBUG') == 'True'  # ← 'True' pas 'Fals'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')  # ← ligne parasite supprimée
+DEBUG = os.environ.get('DEBUG') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -53,7 +53,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600,
-        ssl_require=False  # ← changer True en False
+        ssl_require=False
     )
 }
 
